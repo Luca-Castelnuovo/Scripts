3c2c4fa8668772032ad4dd7c4a5c0a0f435fd9b4
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-08-28 21:56:03 +0200

    updated deploy script

--- scripts/deploy.php
+++ scripts/deploy.php
@@ -1,3 +1,12 @@
 <?php
 
-echo 'deploy script';
+$data = file_get_contents('php://input');
+$signature = hash_hmac('sha1', $data, $configClient->getValue("deploySecret", ""));
+// $data = json_decode($data, true);
+
+if (!hash_equals("sha1={$signature}", $_SERVER['HTTP_X_HUB_SIGNATURE'])) {
+    echo 'invalid signature';
+    exit;
+}
+
+file_put_contents('hooks.log', $data);
