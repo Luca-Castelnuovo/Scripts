3491a2967c30371b0014e3858933d0cca6ac168f
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-08-29 09:53:30 +0200

    update

--- scripts/deploy.php
+++ scripts/deploy.php
@@ -4,11 +4,23 @@ Global $configClient;
 
 $data = file_get_contents('php://input');
 $signature = hash_hmac('sha1', $data, $configClient->getValue("deploySecret", ""));
-// $data = json_decode($data, true);
 
 if (!hash_equals("sha1={$signature}", $_SERVER['HTTP_X_HUB_SIGNATURE'])) {
+    http_response_code(401);
     echo 'invalid signature';
     exit;
 }
 
-file_put_contents('hooks.log', $data);
+$data = json_decode($data, true);
+
+$repoName = $data['repository']['name'];
+
+$repos = [
+    'CDN' => 'cdn.lucacastelnuovo.nl'
+];
+
+// host on test.luca
+
+echo "/var/www/{$repos[$repoName]}";
+
+// exec - git pull /var/www/$repos[$repoName]
